a455ab18426475503c3e03da043b5dff698ff2a4
Matt Butterfield <matt.butterfield@percolate.com>
2015-10-20 22:31:02 -0400

    make it basically work

--- lib.py
+++ lib.py
@@ -2,7 +2,20 @@
 lib for instant_nostalgia
 
 """
+import smtplib
+
+from settings import USER_NAME, PASSWORD, FROM_ADDRESS, SMTP_SERVER
+
+
+TO_ADDRESS_SUFFIX = '@vtext.com'
 
 
 def send_message(phone_number, message):
-    pass
+    to_address = phone_number + TO_ADDRESS_SUFFIX
+    message = '\n' + message
+    server = smtplib.SMTP(SMTP_SERVER)
+    server.starttls()
+    server.login(USER_NAME, PASSWORD)
+    print "SENDING: {} TO: {}".format(message, to_address)
+    server.sendmail(FROM_ADDRESS, to_address, message)
+    server.quit()
---
+++ settings.py
@@ -0,0 +1,8 @@
+import os
+
+
+USER_NAME = os.environ.get("USER_NAME")
+PASSWORD = os.environ.get("PASSWORD")
+FROM_ADDRESS = os.environ.get("FROM_ADDRESS")
+
+SMTP_SERVER = "smtp.gmail.com:587"
