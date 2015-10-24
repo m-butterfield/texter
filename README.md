97e0f7dfa3dc1dd08f653e07132bb1a3632637f2
Matt Butterfield <matt.butterfield@percolate.com>
2015-10-24 12:15:41 -0400

    update readme

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 Texter
 ======
 
-Send text messages using [SMS gateways](https://en.wikipedia.org/wiki/SMS_gateway)
+Send text messages using GMail and [SMS gateways](https://en.wikipedia.org/wiki/SMS_gateway)
 
 ## Setup
 Requires the following environment variables to be set:
 
-    export USER_NAME=<user_name>
-    export PASSWORD=<password>
+    export USER_NAME=<gmail_user_name>
+    export PASSWORD=<gmail_password>
     export FROM_ADDRESS=<from_address>
 
 ## Example Usage
---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'texter',
+    packages = ['texter'],
+    version = '0.1',
+    description = 'Send text messages using SMS gateways',
+    author = 'Matt Butterfield',
+    author_email = 'deneb150@gmail.com',
+    url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+    keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+    classifiers = [],
+)
