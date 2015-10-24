19ef476428f84580980ccb45b03e3e08bd5a0830
Matt Butterfield <matt.butterfield@percolate.com>
2015-10-23 22:35:42 -0400

    moar tests

--- tests.py
+++ tests.py
@@ -11,9 +11,7 @@ from settings import USER_NAME, PASSWORD, FROM_ADDRESS, SMTP_SERVER
 
 CARRIER_NAME = 'verizon'
 PHONE_NUMBER = '1234567890'
-TO_EMAIL = PHONE_NUMBER + CARRIER_GATEWAYS[CARRIER_NAME]
 MESSAGE = 'hello'
-EXPECTED_MESSAGE = '\n' + MESSAGE
 
 
 class TestSendMessage(unittest.TestCase):
@@ -21,11 +19,23 @@ class TestSendMessage(unittest.TestCase):
     @mock.patch("lib.smtplib.SMTP")
     def test_send_message(self, server):
         send_message(PHONE_NUMBER, CARRIER_NAME, MESSAGE)
+        expected_message = '\n' + MESSAGE
+        to_email = PHONE_NUMBER + CARRIER_GATEWAYS[CARRIER_NAME]
         calls = [
             mock.call(SMTP_SERVER),
             mock.call().starttls(),
             mock.call().login(USER_NAME, PASSWORD),
-            mock.call().sendmail(FROM_ADDRESS, TO_EMAIL, EXPECTED_MESSAGE),
+            mock.call().sendmail(FROM_ADDRESS, to_email, expected_message),
             mock.call().quit()
         ]
         self.assertTrue(server.mock_calls == calls)
+
+    def test_invalid_phone_numbers(self):
+        self.assertRaises(
+            ValueError, send_message, '123', CARRIER_NAME, MESSAGE)
+        self.assertRaises(
+            ValueError, send_message, 'tenletters', CARRIER_NAME, MESSAGE)
+
+    def test_invalid_carrier_name(self):
+        self.assertRaises(
+            ValueError, send_message, PHONE_NUMBER, 'blah', MESSAGE)
