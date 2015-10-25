caa1a72bbbfb33eed6626f78c3411f8beb7a9bb8
Matt Butterfield <matt.butterfield@percolate.com>
2015-10-25 16:43:23 -0400

    bump version

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 setup(
     name='texter',
     packages=['texter'],
-    version='0.2',
+    version='0.3',
     description='Send text messages using SMS gateways',
     author='Matt Butterfield',
     author_email='deneb150@gmail.com',
     url='https://github.com/m-butterfield/texter',
-    download_url='https://github.com/m-butterfield/texter/tarball/0.2',
+    download_url='https://github.com/m-butterfield/texter/tarball/0.3',
     keywords=['sms', 'text', 'texting'],
     install_requires=['docopt==0.6.2', 'mock==1.3.0'],
 )
