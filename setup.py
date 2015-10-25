c4df833fef4da015167ff3fa00d1be39c8d2d9f9
Matt Butterfield <matt.butterfield@percolate.com>
2015-10-25 14:20:11 -0400

    pep8

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name = 'texter',
-    packages = ['texter'],
-    version = '0.1',
-    description = 'Send text messages using SMS gateways',
-    author = 'Matt Butterfield',
-    author_email = 'deneb150@gmail.com',
-    url = 'https://github.com/m-butterfield/texter',
-    download_url = 'https://github.com/m-butterfield/texter/tarball/0.1',
-    keywords = ['sms', 'text', 'texting'],
-    classifiers = [],
+    name='texter',
+    packages=['texter'],
+    version='0.1',
+    description='Send text messages using SMS gateways',
+    author='Matt Butterfield',
+    author_email='deneb150@gmail.com',
+    url='https://github.com/m-butterfield/texter',
+    download_url='https://github.com/m-butterfield/texter/tarball/0.1',
+    keywords=['sms', 'text', 'texting'],
 )
