2e7f5746d8225ca12a686e83b7bbf22c851bbfbf
Matt Butterfield <matt.butterfield@percolate.com>
2015-10-25 19:00:00 -0400

    add skeleton for demo site

---
+++ demo_site/resources.py
@@ -0,0 +1,14 @@
+"""
+Resource for Texts
+
+"""
+from flask.ext.restful import Resource
+
+
+class TextResource(Resource):
+    """
+    Api Resource for texts
+
+    """
+    def post(self):
+        return {}
---
+++ demo_site/views.py
@@ -0,0 +1,17 @@
+"""
+Views for the application
+
+"""
+from flask import render_template
+
+from demo_site import app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'Not found :(', 404
