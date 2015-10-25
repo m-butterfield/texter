c0a16714c2bb03dd991341d5381cf2cc6ffdc1b4
Matt Butterfield <matt.butterfield@percolate.com>
2015-10-25 15:08:44 -0400

    0.2

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ requirements = [str(ir.req) for ir in parse_requirements('requirements.txt')]
 setup(
     name='texter',
     packages=['texter'],
-    version='0.1',
+    version='0.2',
     description='Send text messages using SMS gateways',
     author='Matt Butterfield',
     author_email='deneb150@gmail.com',
     url='https://github.com/m-butterfield/texter',
-    download_url='https://github.com/m-butterfield/texter/tarball/0.1',
+    download_url='https://github.com/m-butterfield/texter/tarball/0.2',
     keywords=['sms', 'text', 'texting'],
     install_requires=requirements,
 )
