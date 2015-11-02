34aa388c7bb86aa57eec6b2e6686a4b9f7c153c7
Matt Butterfield <matt.butterfield@percolate.com>
2015-11-01 21:38:35 -0500

    add app_settings and sqlalchemy db

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask.ext.restful import Api
+from flask.ext.sqlalchemy import SQLAlchemy
+
+from app_settings import DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+db = SQLAlchemy(app)
 api = Api(app)
 
 
---
+++ app_settings.py
@@ -0,0 +1,4 @@
+import os
+
+DATABASE_URI = os.getenv('DATABASE_URI')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
